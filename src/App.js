a0750d00e703c6f903983e12e3f88691cd054f52
sedx876 <swatkins876@gmail.com>
2020-12-05 09:32:56 -0500

    Added ActionProvider and MessageParser files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Chatbot from 'react-chatbot-kit'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+        <Chatbot/>
+      </header>
     </div>
   );
 }
